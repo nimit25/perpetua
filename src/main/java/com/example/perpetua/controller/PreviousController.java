2bf8977b2a1985182b285e3ea084881e589cf54b
Nimit Sachdeva <31580737+nimit25@users.noreply.github.com>
2021-08-11 17:30:55 -0400

    Add files via upload

--- src/main/java/com/example/perpetua/controller/PreviousController.java
+++ src/main/java/com/example/perpetua/controller/PreviousController.java
@@ -1,77 +1,49 @@
 package com.example.perpetua.controller;
 
-import com.example.perpetua.dto.Playlist;
 import com.example.perpetua.dto.Song;
-import com.example.perpetua.repository.PlaylistRepo;
-import com.example.perpetua.repository.SongRepo;
 import com.example.perpetua.services.MusixmatchService;
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 @RestController
-public class PlaylistController {
+@RequestMapping("/previous")
+public class PreviousController {
 
     final static Logger logger = LoggerFactory.getLogger(PlaylistController.class);
-
-    @Autowired
-    SongRepo songRepo;
-
-    @Autowired
-    PlaylistRepo playlistRepo;
-
     @Autowired
     MusixmatchService musixmatchService;
+    List<Song> result = new ArrayList<>();
 
-
-    @GetMapping("/getSubsequentSong/{id}")
-    public Playlist getSubsequentSong(@PathVariable String id,  @RequestParam String apikey){
-        logger.info("fetching subsequent song for playlist " + id);
-        Playlist playlist = playlistRepo.findById(Long.valueOf(id)).get();
-        List<Song> result = playlist.getSongs();
+    @GetMapping("/getSubsequentSong")
+    public List<Song> getlyrics(@RequestParam String apikey){
+        logger.info("fetching subsequent song");
         String randomLyrics = getRandomWords(result.get(0));
-        logger.info("random lyrics were " + randomLyrics);
         Song song = musixmatchService.fetchSong(randomLyrics, apikey);
-        if (song != null){
-            songRepo.save(song);
-            result.remove(0);
-            result.add(song);
-            playlist.setSongs(result);
-
-            playlistRepo.save(playlist);
-            return playlist;
+        if (song == null){
+            return new ArrayList<Song>();
         }
-
         result.remove(0);
-        playlist.setSongs(result);
-        playlistRepo.save(playlist);
-        return playlist;
-
-
+        result.add(song.clone());
+        return result;
     }
 
 
     @GetMapping("/generatePlaylist")
-    public Playlist generatePlaylist( @RequestParam String apikey)  {
+    public List<Song> generatePlaylist( @RequestParam String apikey)  {
         logger.info("generating playlist");
-        List<Song> result = new ArrayList<>();
+        result = new ArrayList<>();
         Song song1 =  musixmatchService.fetchSong("Any word in the lyrics", apikey);
-        Song song2 = nextSong(song1, apikey);
-//        Song song2 = musixmatchService.fetchSong("Any word in the lyrics", apikey, 1);
+        Song song2 = musixmatchService.fetchSong("Any word in the lyrics", apikey);
         result.add(song1);
         result.add( song2);
-        songRepo.saveAll(result);
-        Playlist playlist = new Playlist(result);
-        playlistRepo.save(playlist);
 
 //        String randomLyrics = getRandomWords(song1);
 //        Song song = musixmatchService.fetchSong(randomLyrics, apikey, 0);
@@ -82,11 +54,11 @@ public class PlaylistController {
 //            String randomLyrics1 = getRandomWords(songBuff);
 //            song = musixmatchService.fetchSong(randomLyrics1, apikey, 0);
 //        }
-        return playlist;
+        return result;
     }
 
     @GetMapping("/getSong")
-    public Song  getLyrics(@RequestParam String lyrics, @RequestParam String apikey){
+    public Song  getlyrics(@RequestParam String lyrics, @RequestParam String apikey){
         logger.info("fetching song");
         Song song = musixmatchService.fetchSong(lyrics, apikey);
         if (song == null){
@@ -98,7 +70,7 @@ public class PlaylistController {
     public String getRandomWords(Song song1) {
         logger.info("getting random words");
         List<String> str2 = new ArrayList<>();
-        for (String token : song1.getLyrics().split("\\W+") ){// Array.asList(
+        for (String token : song1.getLyrics().split("\\W+") ){
             str2.add(token);
         }
         List<String> listOfWords = str2.subList(0, str2.size() - 8); // removing "this is not for commercial use " from the end
@@ -117,12 +89,6 @@ public class PlaylistController {
         }
 
         return result.toString();
-    }
-
-    public Song nextSong(Song song, String apikey){
-        String randomLyrics = getRandomWords(song);
-        Song song2 = musixmatchService.fetchSong(randomLyrics, apikey);
-        return song;
     }
 
 }
